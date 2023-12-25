02b5623214d002c8444b2118446814b2bd641720
Adolf Mathebula <Adolfm@intenda.za.com>
2023-12-25 14:05:12 +0200

    Feature: Implemented dao layer

--- src/main/java/com/adolf/springdemo/student/StudentController.java
+++ src/main/java/com/adolf/springdemo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.adolf.springdemo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +15,34 @@ public class StudentController {
     @Autowired //can be removed cos of new Spring updates
     public StudentController(StudentService service) {
         this.service = service;
+    }
+
+    @PostMapping
+    public Student save(
+           @RequestBody Student student
+    ){
+        return service.save(student);
+    }
+
+    @GetMapping("/{email}")
+    public Student findByEmail(
+           @PathVariable("email") String email
+    ) {
+        return service.findByEmail(email);
+    }
+
+    @PutMapping
+    public Student updateStudent(
+            @RequestBody Student student
+    ){
+        return service.update(student);
+    }
+
+    @DeleteMapping("/{email}")
+    public void  delete(
+           @PathVariable("email") String email
+    ){
+        service.delete(email);
     }
 
     @GetMapping
--- src/main/java/com/adolf/springdemo/student/StudentService.java
+++ src/main/java/com/adolf/springdemo/student/StudentService.java
@@ -5,25 +5,13 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
-    public List<Student> findAllStudents(){
-        return List.of(
-                new Student(
-                        "Adolf",
-                        "Mathebula",
-                        LocalDate.now(),
-                        "contact@adolf.co.za",
-                        32
-                ),
-                new Student(
-                        "Joe",
-                        "Smith",
-                        LocalDate.now(),
-                        "contact@joe.com",
-                        28
-                )
 
-        );
-    }
+public interface StudentService {
+
+    Student save(Student s);
+    List<Student> findAllStudents();
+
+    Student findByEmail(String email);
+    Student update(Student s);
+    void delete(String email);
 }
